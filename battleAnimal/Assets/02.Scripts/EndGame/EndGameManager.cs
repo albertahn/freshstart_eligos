e64c52b093ba7b19a2b6503ed08b3ac3cd309d4f
contendera <contendera@gmail.com>
2015-06-08 20:17:23 +0900

    endgamechange

---
+++ battleAnimal/Assets/02.Scripts/EndGame/EndGameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGameManager : MonoBehaviour {
+
+	public GameObject stat_row;
+
+
+	// Use this for initialization
+	void Start () {
+
+		for (int i=0; i<7; i++) {
+				
+		
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
