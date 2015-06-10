2ed7b6c4a9fad4ca03c0d79734a71ff436c71691
contendera <contendera@gmail.com>
2015-06-10 09:57:59 +0900

    changed_endGame

--- battleAnimal/Assets/02.Scripts/EndGame/EndGameManager.cs
+++ battleAnimal/Assets/02.Scripts/EndGame/EndGameManager.cs
@@ -1,55 +1,96 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+
+using Boomlagoon.JSON;
 
 public class EndGameManager : MonoBehaviour {
 
+
+
 	public GameObject stat_row, pannel_content;
 
+	private EndServerDatabase endServerDatabase;
+
 	public int itemCount = 10, columnCount = 1;
+
+	RectTransform containerRectTransform ;
 
 	// Use this for initialization
 	void Start () {
 
-		RectTransform containerRectTransform = pannel_content.GetComponent<RectTransform>();
+		endServerDatabase = GetComponent<EndServerDatabase> ();
+
+		StartCoroutine (GetStatData ("1"));//ClientState.room));
+
+//		containerRectTransform = pannel_content.GetComponent<RectTransform>();
 		
 		//calculate the width and height of each child item.
 		//float width = containerRectTransform.rect.width / columnCount;
 
-		float height = 100;
-		int rowCount = itemCount / columnCount;
+
+		//int rowCount = itemCount / columnCount;
 
 
 
-		containerRectTransform.sizeDelta = new Vector2( 20, 100*7);
-
-		for (int i=0; i<7; i++) {
-		
 
 
-			//create a new item, name it, and set the parent
-			GameObject newItem = Instantiate(stat_row) as GameObject;
-			//GameObject pannelContent = Instantiate(pannel_content) as GameObject;
 
-			newItem.name = stat_row.name + " item at (" + i  + ")";
-			newItem.transform.parent = pannel_content.transform;
-			
-			//move and size the new item
-			RectTransform rectTransform = newItem.GetComponent<RectTransform>();
+	
+	}
+	
+
+	private IEnumerator GetStatData (string roomIndex)
+	{		
+		yield return StartCoroutine (endServerDatabase.getRoomStats(roomIndex)); // id를 Email로 바꿔야 하지 않을까
+
+		Debug.Log("kills:  "+ endServerDatabase.fuckArray.Length);	
+
+		//containerRectTransform.sizeDelta = new Vector2( 20, 100*endServerDatabase.fuckArray.Length);
+
+		for ( int i =0; i< endServerDatabase.fuckArray.Length; i ++){
+
+			JSONObject jsonobj = JSONObject.Parse(endServerDatabase.fuckArray[i].ToString());
+
+			setStatRow (i, jsonobj.GetString("kills"),jsonobj.GetString("assists"),jsonobj.GetString("deaths"),jsonobj.GetString("points") );
 
 
-			float y = containerRectTransform.rect.height / 2 - height * i;
-			rectTransform.offsetMin = new Vector2(0, y);
-			
-
-			y = rectTransform.offsetMin.y + height;
-			rectTransform.offsetMax = new Vector2(0, y);
-		
 		}
-	
+
+
+		yield return null;		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+
+	public void setStatRow(int num, string kills, string assists, string deaths, string points){
+
+
+		Debug.Log ("stats: "+kills+"assis: "+assists);
+
+
+		//create a new item, name it, and set the parent
+
+		GameObject newItem = GameObject.Find ("stat_row_red1");
+
+		//GameObject newItem = Instantiate(stat_row) as GameObject;
+		//GameObject pannelContent = Instantiate(pannel_content) as GameObject;
+
+		newItem.transform.FindChild ("kills_tx").transform.GetComponent<Text> ().text = kills;
+		newItem.transform.FindChild ("points_tx").transform.GetComponent<Text> ().text = points;
+
+
+		//Debug.Log ("len"+kills_tx.GetComponent<Text>());
+
+		//textmesh[0].text = "hi";
+		
+		newItem.name = stat_row.name + " item at ()";
+		//newItem.transform.parent = pannel_content.transform;
+		
+//		newItem.transform.SetParent (pannel_content.transform);
+
+
+
+
 	}
+
 }
---
+++ battleAnimal/Assets/02.Scripts/EndGame/EndServerDatabase.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Boomlagoon.JSON;
+
+
+public class EndServerDatabase : MonoBehaviour {
+	public JSONArray fuckArray;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		fuckArray = new JSONArray();
+
+
+	
+	}
+
+
+	public IEnumerator getRoomStats(string roomindex)
+	{
+		string url = "http://mobile.sharebasket.com/room/get_game_stats/"+roomindex;
+		
+		// Create a form object for sending high score data to the server
+		WWWForm form = new WWWForm();
+		// Assuming the perl script manages high scores for different games
+		
+		// The name of the player submitting the scores
+
+		
+		// Create a download object
+		WWW downloadbabe = new WWW( url );
+		// Wait until the download is done
+		yield return downloadbabe;
+		if(downloadbabe.error !=null) {
+			
+			Debug.Log( "Error downloading: " + downloadbabe.error );
+			//return;
+		} else {
+			// show the highscores
+			Debug.Log(downloadbabe.text);
+		}
+		
+		//WWW www = new WWW (url);
+		//yield return www;
+		
+		if (downloadbabe.size <= 2) {
+			
+			yield return null;
+			
+		} else {
+			
+			fuckArray = JSONArray.Parse(downloadbabe.text);
+		}
+		
+		
+	}//end getdata
+
+
+
+	public IEnumerator sendRoomStats(string roomindex, string[]  allStats )
+	{
+		string url = "http://mobile.sharebasket.com/room/get_game_stats/"+roomindex;
+		
+		// Create a form object for sending high score data to the server
+		WWWForm form = new WWWForm();
+		// Assuming the perl script manages high scores for different games
+		
+		// The name of the player submitting the scores
+		form.AddField( "index",  "index");
+		form.AddField( "email", "email" );
+		form.AddField( "score",   "score");
+		
+		// Create a download object
+		WWW downloadbabe = new WWW( url, form );
+		// Wait until the download is done
+		yield return downloadbabe;
+		if(downloadbabe.error !=null) {
+			
+			Debug.Log( "Error downloading: " + downloadbabe.error );
+			//return;
+		} else {
+			// show the highscores
+			Debug.Log(downloadbabe.text);
+		}
+		
+		//WWW www = new WWW (url);
+		//yield return www;
+		
+		if (downloadbabe.size <= 2) {
+			
+			yield return null;
+			
+		} else {
+			
+			fuckArray = JSONArray.Parse(downloadbabe.text);
+		}
+		
+		
+	}//end getdata
+
+
+
+}
